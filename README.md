7ef39ba8d7a49cbda2a58d0cd331b82de457b4c1
Eoin O'Brien <eoinobrien910@gmail.com>
2018-11-06 00:17:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# ul-exam-scheduler
+
+Finds mutually free timetable slots for students at the University of Limerick.
+
+## Getting started
+
+Clone and install dependencies:
+
+```shell
+git clone https://github.com/eoin-obrien/ul-exam-scheduler.git # via HTTPS
+git clone git@github.com:eoin-obrien/ul-exam-scheduler.git # via SSH
+cd ul-exam-scheduler
+npm install
+```
+
+## Usage
+
+Find mutually free slots in week 10 for student 44556677 and student 11223344:
+
+```shell
+node index.js 10 44556677 11223344
+```
+
+## License
+
+Licensed under the GNU GNU General Public License v3.0 (see LICENSE).
---
+++ index.js
@@ -0,0 +1,117 @@
+#!/usr/bin/env node
+
+'use strict';
+
+
+/*
+ * Imports
+ */
+
+const studentTimetable = require('ul-timetable').studentTimetable;
+const Day = require('ul-timetable').Day;
+
+
+/*
+ * Globals
+ */
+
+const weekPattern = /^([1-9]|1[1-3])$/;     // validates a UL student ID
+const studentIdPattern = /^([0-9]{7,8})$/;  // validates a UL student ID
+const fromHour = 9;                         // first available slot starts at 09:00
+const toHour = 18;                          // last available slot finishes at 18:00
+const daysInWeek = 5;                       // 5 to ignore Saturday timetables, 6 otherwise
+
+
+/*
+ * Utility functions
+ */
+
+// Converts a slot time to an hour index.
+const timeToIndex = (time) => parseInt(time.split(':')[0], 10) - fromHour;
+
+// Converts an hour index to a 24-hour clock time.
+const indexToTime = (index) => (index + fromHour).toString().padStart(2, 0) + ':00';
+
+// Returns an array of invalid student IDs from the input array.
+const findInvalidStudentIds = (ids) => ids.filter(id => !studentIdPattern.test(id));
+
+// Counts the number of truthy elements in the input array.
+const trueCount  = (array) => array.filter(element => element).length;
+
+// Counts the number of available slots remaining.
+const countAvailableSlots = (slots) => slots.reduce((prev, curr) => prev + trueCount(curr), 0);
+
+
+/*
+ * Arguments
+ */
+
+// Validate week number input
+const week = process.argv[2];
+if (!weekPattern.test(week)) {
+    console.log(`Invalid week number: ${week}`);
+    process.exit(1);
+}
+
+// Validate student ID input
+const studentIds = process.argv.slice(3);   // all remaining command-line arguments
+const invalidIds = findInvalidStudentIds(studentIds);
+if (invalidIds.length) {
+    for (const id of invalidIds) {
+        console.log(`Invalid student ID: ${id}`);
+    }
+    process.exit(1);
+}
+
+
+/*
+ * Main algorithm
+ */
+
+(async () => {
+    // Create 2D array to represent available slots per day
+    const slotsPerDay = toHour - fromHour;
+    const slots = new Array(daysInWeek).fill(null).map(() => new Array(slotsPerDay).fill(true));
+
+    // Query each student's timetable and update available slots
+    let idCount = 0;
+    for (const id of studentIds) {
+        console.log(`Querying week ${week} timetable for student ${id}... (${++idCount}/${studentIds.length})`);
+
+        // Query timetable
+        const timetable = await studentTimetable(id);
+
+        // Iterate over lessons and set those slots to false
+        for (let day = 0; day < daysInWeek; day++) {
+            // Filter lessons by week number
+            const lessons = timetable.lessons[day].filter(lesson => lesson.weekIds.includes(week));
+
+            // Mark conflicting slots as unavailable
+            for (const lesson of lessons) {
+                const fromIndex = timeToIndex(lesson.fromTime);
+                const toIndex = timeToIndex(lesson.toTime);
+                for (let i = fromIndex; i < toIndex; i++) {
+                    slots[day][i] = false;
+                }
+            }
+        }
+        
+        // Exit if there are no free slots remaining
+        const availableSlots = countAvailableSlots(slots);
+        console.log(`Available slots: ${availableSlots}`);
+        if (!availableSlots) {
+            process.exit();
+        }
+    }
+
+    // Print available slot times
+    console.log(`Final available slots: ${countAvailableSlots(slots)}`);
+    for (let day = 0; day < daysInWeek; day++) {
+        for (let slot = 0; slot < slotsPerDay; slot++) {
+            if (slots[day][slot]) {
+                console.log(`${Day[day]}, ${indexToTime(slot)}-${indexToTime(slot + 1)}`);
+            }
+        }
+    }
+
+})();
